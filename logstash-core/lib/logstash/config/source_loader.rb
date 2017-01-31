24f256ffb158e9ba399bea2a0b94def964b17952
Pier-Hugues Pellerin <phpellerin@gmail.com>
2017-01-31 13:03:41 -0500

    more refactoring, still some errors in the tests

--- logstash-core/lib/logstash/config/source_loader.rb
+++ logstash-core/lib/logstash/config/source_loader.rb
@@ -6,61 +6,60 @@ require "set"
 
 module LogStash module Config
   class SourceLoader
-    class Loader
-      def initialize(loaders)
-        @loaders = loaders
+    class AggregateSource
+      def initialize(sources)
+        @sources = sources
       end
 
       def fetch
-        @loaders.collect(&:pipeline_configs).flatten
+        @sources.collect(&:pipeline_configs).flatten
       end
     end
 
     include LogStash::Util::Loggable
 
     def initialize
-      @source_loaders_mutex = Mutex.new
-      @source_loaders = Set.new([LogStash::Config::Source::Local])
+      @sources_lock = Mutex.new
+      @sources = Set.new([LogStash::Config::Source::Local])
     end
 
     # This return a ConfigLoader object that will
     # abstract the call to the different sources and will return multiples pipeline
     def create(settings)
-      loaders = []
+      source_loaders = []
 
-      source_loaders do |config_source|
-        if config_source.match?(settings)
-          loaders << config_source.new(settings)
+      sources do |source|
+        if source.match?(settings)
+          source_loaders << source.new(settings)
         end
       end
 
-      if loaders.empty?
-
+      if sources.empty?
         # This shouldn't happen with the settings object or with any external plugins.
         # but lets add a guard so we fail fast.
         raise LogStash::InvalidSourceLoaderSettingError, "Can't find an appropriate config loader with current settings"
       else
-        Loader.new(loaders)
+        Source.new(sources)
       end
     end
 
-    def source_loaders
-      @source_loaders_mutex.synchronize do
-        @source_loaders.each do |config|
-          yield config
+    def sources
+      @sources_lock.synchronize do
+        @sources.each do |source|
+          yield source
         end
       end
     end
 
-    def configure_sources(sources)
-      sources = Array(sources).to_set
-      logger.debug("Configure sources", :sources => sources.collect(&:to_s))
-      @source_loaders_mutex.synchronize { @source_loaders = sources }
+    def configure_sources(new_sources)
+      new_sources = Array(new_sources).to_set
+      logger.debug("Configure sources", :sources => new_sources.collect(&:to_s))
+      @sources_lock.synchronize { @sources = new_sources }
     end
 
-    def add_source(source)
-      logger.debug("Adding source", :source => source.to_s)
-      @source_loaders_mutex.synchronize { @source_loaders << source}
+    def add_source(new_source)
+      logger.debug("Adding source", :source => new_source.to_s)
+      @sources_lock.synchronize { @sources << new_source}
     end
   end
 
