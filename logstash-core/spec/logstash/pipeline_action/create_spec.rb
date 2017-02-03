644db689c1de579a54bec59fb8b44f4fa4645e22
Pier-Hugues Pellerin <phpellerin@gmail.com>
2017-02-03 16:54:28 -0500

    adding unit tests for actions

--- logstash-core/spec/logstash/pipeline_action/create_spec.rb
+++ logstash-core/spec/logstash/pipeline_action/create_spec.rb
@@ -2,15 +2,50 @@
 require "spec_helper"
 require_relative "../../support/helpers"
 require "logstash/pipeline_action/create"
+require "logstash/Instrument/collector"
 require "logstash/instrument/null_metric"
+require "logstash/inputs/generator"
 
 describe LogStash::PipelineAction::Create do
-  let(:metric) { LogStash::Instrument::NullMetric.new }
-  let(:pipeline_config) { mock_pipeline_config(:main) }
+  let(:metric) { LogStash::Instrument::NullMetric.new(LogStash::Instrument::Collector.new) }
+  let(:pipeline_config) { mock_pipeline_config(:main, "input { generator { id => '123' } } output { stdout {} }") }
+  let(:pipelines) {  Hash.new }
 
   subject { described_class.new(pipeline_config, metric) }
 
   it "returns the pipeline_id" do
     expect(subject.pipeline_id).to eq(:main)
+  end
+
+  context "when the pipeline succesfully start" do
+    it "adds the pipeline to the current pipelines" do
+      expect { subject.execute(pipelines) }.to change(pipelines, :size).by(1)
+    end
+
+    it "starts the pipeline" do
+      subject.execute(pipelines)
+      expect(pipelines[:main].running?).to be_truthy
+    end
+
+    it "returns a successful execution status" do
+      expect(subject.execute(pipelines)).to be_truthy
+    end
+  end
+
+  context  "when the pipeline doesn't start" do
+    context "with a syntax error" do
+      let(:pipeline_config) { mock_pipeline_config(:main, "input { generator { id => '123' } } output { stdout ") } # bad syntax
+
+      it "raises the exception upstream" do
+        expect { subject.execute(pipelines) }.to raise_error
+      end
+    end
+
+    context "with an register or validation error" do
+      it "returns false" do
+        allow_any_instance_of(LogStash::Inputs::Generator).to receive(:register).and_raise("Bad value")
+        expect(subject.execute(pipelines)).to be_falsey
+      end
+    end
   end
 end
--- logstash-core/spec/logstash/pipeline_action/stop_spec.rb
+++ logstash-core/spec/logstash/pipeline_action/stop_spec.rb
@@ -2,15 +2,35 @@
 require "spec_helper"
 require_relative "../../support/helpers"
 require "logstash/pipeline_action/stop"
+require "logstash/pipeline"
 require "logstash/instrument/null_metric"
 
 describe LogStash::PipelineAction::Stop do
-  let(:metric) { LogStash::Instrument::NullMetric.new }
-  let(:pipeline_config) { mock_pipeline_config(:main) }
+  let(:metric) { LogStash::Instrument::NullMetric.new(LogStash::Instrument::Collector.new) }
+  let(:pipeline_config) { "input { generator {} } output { }" }
+  let(:pipeline_id) { :main }
+  let(:pipeline) { LogStash::Pipeline.new(pipeline_config) }
+  let(:pipelines) { { :main => pipeline } }
 
-  subject { described_class.new(pipeline_config, metric) }
+  subject { described_class.new(pipeline_id) }
+
+  before do
+    pipeline.start
+  end
 
   it "returns the pipeline_id" do
     expect(subject.pipeline_id).to eq(:main)
+  end
+
+  it "shutdown the running pipeline" do
+    expect(pipeline.running?).to be_truthy
+    subject.execute(pipelines)
+    expect(pipeline.running?).to be_falsey
+  end
+
+  it "removes the pipeline from the running pipelines" do
+    expect(pipelines.include?(pipeline_id)).to be_truthy
+    subject.execute(pipelines)
+    expect(pipelines.include?(pipeline_id)).to be_falsey
   end
 end
